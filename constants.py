1240d2cf58cdaf07b87488431266e9498efde69c
ankh6 <andynzemokalumu@live.be>
2022-06-29 22:54:05 +0200

    Added constant variables

---
+++ constants.py
@@ -0,0 +1,6 @@
+BINANCE_API_BASE_URL = "https://api.binance.com/api"
+BINANCE_API_VERSION = "v3"
+KUCOIN_API_BASE_URL = "https://api.kucoin.com/api"
+KUCOIN_API_VERSION = "v1"
+SPOTTER_RUNNING_FREQUENCY = 60
+SUPPORTED_STABLECOINS = ("BUSD", "USDC", "USDT")
