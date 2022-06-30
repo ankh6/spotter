935f53d1f6e83e520ba942aab43f6fdd3bcd58a9
ankh6 <andynzemokalumu@live.be>
2022-06-30 19:10:23 +0200

    added types for variables

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-BINANCE_API_BASE_URL = "https://api.binance.com/api"
-BINANCE_API_VERSION = "v3"
-KUCOIN_API_BASE_URL = "https://api.kucoin.com/api"
-KUCOIN_API_VERSION = "v1"
-SPOTTER_RUNNING_FREQUENCY = 60
+BINANCE_API_BASE_URL: str = "https://api.binance.com/api"
+BINANCE_API_VERSION: str = "v3"
+KUCOIN_API_BASE_URL: str = "https://api.kucoin.com/api"
+KUCOIN_API_VERSION: str = "v1"
+SPOTTER_RUNNING_FREQUENCY: float = 60.0
 SUPPORTED_STABLECOINS = ("BUSD", "USDC", "USDT")
