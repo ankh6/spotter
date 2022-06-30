4bb7ea1a4dd1606f9f52843e7fe25b37a714ea68
ankh6 <andynzemokalumu@live.be>
2022-06-30 14:45:00 +0200

    Added import Added another column into the csv file, the name of the exchange. It will be appreciated for data analysis

--- services/reporter.py
+++ services/reporter.py
@@ -1,11 +1,12 @@
 from csv import Error, writer
+from interfaces.Exchange import Exchange
 import sys
 
-def _create_report(date, trading_pair: str, bid, ask, spread_percentage):
+def _create_report(date, trading_pair: str, bid: float, ask: float, spread_percentage: float, exchange: Exchange):
     with open(f"./reports/{trading_pair}.csv", mode="a", newline="\n") as file:
         try:
             _writer = writer(file)
-            row = [date,trading_pair,bid,ask,spread_percentage]
+            row = [date,trading_pair,bid,ask,spread_percentage, exchange]
             _writer.writerow(row)
         except Error as csv_error:
             sys.exit(f"file {file}\n{csv_error}")
