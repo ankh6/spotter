5cbb4e201579cfd644b113b88a8e2f726601f0dc
ankh6 <andynzemokalumu@live.be>
2022-06-30 19:09:50 +0200

    Implemented asynchronous behavior when interacting with Kucoin and Binance APIs

--- spotter.py
+++ spotter.py
@@ -5,12 +5,11 @@ from interfaces.MarketRetriever import MarketRetriever
 from markets.BinanceMarketRetriever import BinanceMarketRetriever
 from markets.KuCoinMarketRetriever import KuCoinMarketRetriever
 from os import mkdir
-import sched    
-from time import sleep, time
 from typing import Dict, List, Union
 from utils import MarketRetrieverUtils
+import asyncio
 
-def initialize():
+async def initialize():
     '''' Initialization of the spotter
 
     Returns:
@@ -21,9 +20,9 @@ def initialize():
     binance: MarketRetriever = BinanceMarketRetriever()
     kucoin: MarketRetriever = KuCoinMarketRetriever()
     print("Fetching markets from Binance. . .")
-    binance.fetch_trading_pairs()
+    await binance.fetch_trading_pairs()
     print("Fetching markets from KuCoin. . .")
-    kucoin.fetch_trading_pairs()
+    await kucoin.fetch_trading_pairs()
     binance_symbols: List[str] = binance._get_trading_pairs()
     kucoin_symbols: List[str] = kucoin._get_trading_pairs()
     print("Fetching markets that are on both Binance and KuCoin. . .")
@@ -32,7 +31,7 @@ def initialize():
     return (binance, kucoin, union_markets_set)
 
 
-def monitor_spread_on_exchanges(exchanges, trading_pairs: List[str]):
+async def monitor_spread_on_exchanges(exchanges, trading_pairs: List[str]):
     ''' Given the exchanges and the trading pairs, it fetches the tickers that are
     time, trading pair, highest bid, lowest ask, spread percentage and the name of the exchange
 
@@ -46,7 +45,7 @@ def monitor_spread_on_exchanges(exchanges, trading_pairs: List[str]):
             raise UnsupportedExchangeError(f"The exchange {exchange.exchange_name} is not supported")
         try:
             print(f"Fetching highest bid and lowest ask on {exchange.exchange_name}. . .")
-            tickers: Dict[str,List[Union[int,str]]] = exchange.fetch_tickers(trading_pairs)
+            tickers: Dict[str,List[Union[int,str]]] = await exchange.fetch_tickers(trading_pairs)
             print(f"Building report. . .")
             MarketRetrieverUtils.generate_trading_report(tickers, exchange.exchange_name)
         except Exception as e:
@@ -55,7 +54,7 @@ def monitor_spread_on_exchanges(exchanges, trading_pairs: List[str]):
         print("Created reports located inside './reports' folder")
 
 
-def execute_monitoring(exchanges: List[MarketRetriever], union_markets_set: List[str]):
+async def execute_monitoring(exchanges: List[MarketRetriever], union_markets_set: List[str]):
     '''
     Wrapper for the execution of the monitoring.
     Executes every minute, value of SPOTTER_RUNNING_FREQUENCY
@@ -64,14 +63,12 @@ def execute_monitoring(exchanges: List[MarketRetriever], union_markets_set: List
     exchanges: the list of exchanges that have been initialized by initialize()
     union_markets_set: the list of of markets that are on both Binance and KuCoin
     '''
-    monitor_spread_on_exchanges(exchanges=exchanges, trading_pairs=union_markets_set)
-    scheduler = sched.scheduler(time, sleep)
-    # priority = 1
-    # Mandatory argument, it equals the highest priority
-    scheduler.enter(delay=SPOTTER_RUNNING_FREQUENCY, priority=1, action=execute_monitoring,kwargs={"exchanges":exchanges, "union_markets_set":union_markets_set})
-    scheduler.run()
-
+    await monitor_spread_on_exchanges(exchanges=exchanges, trading_pairs=union_markets_set)
+    while True:
+        print((f"Waiting for {SPOTTER_RUNNING_FREQUENCY} seconds before next run"))
+        await asyncio.sleep(SPOTTER_RUNNING_FREQUENCY)
+        await execute_monitoring(exchanges=exchanges, union_markets_set=union_markets_set)
 
 if __name__ == '__main__':
-    binance, kucoin, markets = initialize()
-    execute_monitoring([binance, kucoin], markets)
+    binance, kucoin, markets = asyncio.run(initialize())
+    asyncio.run(execute_monitoring([binance, kucoin], markets))
